e5b7b28c0ac4ae73d95f43ab0c2bd13049f23bc9
Tao <ctaohe@gmail.com>
2019-08-21 01:15:12 +0800

    Initialize sequelize connect to database

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const db = require('./models')
+
 const exphbs = require('express-handlebars')
 
-app.engine('handlebars', exphbs())
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`Example app listening on port ${port}`)
 })
+
+require('./routes')(app)
