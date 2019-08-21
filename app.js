68cc361f8bcfe90f1a3b827440b2b9357ac2ac43
Tao <ctaohe@gmail.com>
2019-08-22 00:42:18 +0800

    Install body parser

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 const db = require('./models')
-
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.listen(port, () => {
   db.sequelize.sync()
