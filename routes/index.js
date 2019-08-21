fa497a1ff02df3ce31819807afd63ed1be6a5f2a
Tao <ctaohe@gmail.com>
2019-08-22 00:44:05 +0800

    Add signup page route and signup route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 module.exports = app => {
   app.get('/', (req, res) => res.redirect('restaurants'))
   app.get('/restaurants', restController.getRestaurants)
 
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', adminController.getRestaurants)
+
+  app.get('/signup', userController.singUpPage)
+  app.post('/signup', userController.signUp)
 }
