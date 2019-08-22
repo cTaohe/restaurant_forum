622cb1ae77a6bdbb745d248618ddf51c3b21ef86
Tao <ctaohe@gmail.com>
2019-08-22 22:27:49 +0800

    Install passport and passport-local add signin page and sign signout

--- app.js
+++ app.js
@@ -4,16 +4,33 @@ const port = 3000
 const db = require('./models')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('./config/passport')
 
-
+// setup handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+// setup bodyParser
 app.use(bodyParser.urlencoded({ extended: true }))
+// setup session
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+// setup passport
+app.use(passport.initialize())
+app.use(passport.session())
+// setup flash
+app.use(flash())
 
+// res.flash in res.locals
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
   console.log(`Example app listening on port ${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
