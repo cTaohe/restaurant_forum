e4a2b8125af5e1690013edf26846d425a64dacfe
Tao <ctaohe@gmail.com>
2019-08-26 21:25:35 +0800

    Add pagination on restaurant index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,16 +1,27 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
+
 let restController = {
   getRestaurants: (req, res) => {
+    let offset = 0
     let whereQuery = {}
     let categoryId = ''
+    if(req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery['categoryId'] = categoryId
     }
-    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
-      const data = restaurants.map(r => ({
+    Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset:offset, limit:pageLimit }).then(result => {
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      let prev = page-1 < 1 ? 1 : page - 1
+      let next = page+1 > pages ? pages : page + 1
+      const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50)
       }))
@@ -19,11 +30,16 @@ let restController = {
         return res.render('restaurants', {
           restaurants: data,
           categories: categories,
-          categoryId: categoryId
+          categoryId: categoryId,
+          page: page,
+          totalPage: totalPage,
+          perv: prev,
+          next: next
         })
       })
     })
   },
+
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category
