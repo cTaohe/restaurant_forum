2375822fba64cdbc1f280a0cf6b53d644bbc863f
Tao <ctaohe@gmail.com>
2019-08-29 00:31:01 +0800

    Add to count view feature of restaurant page

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,6 +2,7 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 const Comment = db.Comment
+const Favorite = db.Favorite
 const User = db.User
 const pageLimit = 10
 
@@ -10,19 +11,19 @@ let restController = {
     let offset = 0
     let whereQuery = {}
     let categoryId = ''
-    if(req.query.page) {
+    if (req.query.page) {
       offset = (req.query.page - 1) * pageLimit
     }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery['categoryId'] = categoryId
     }
-    Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset:offset, limit:pageLimit }).then(result => {
+    Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit }).then(result => {
       let page = Number(req.query.page) || 1
       let pages = Math.ceil(result.count / pageLimit)
       let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
-      let prev = page-1 < 1 ? 1 : page - 1
-      let next = page+1 > pages ? pages : page + 1
+      let prev = page - 1 < 1 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
       const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
@@ -47,18 +48,35 @@ let restController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
-        { model: User, as: 'FavoritedUsers'},
+        { model: User, as: 'FavoritedUsers' },
         { model: Comment, include: [User] }
       ]
-    }).then(restaurant => {
-      console.log(req.user)
-      const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)      
+    })
+    .then(restaurant => {
+      const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)
+      restaurant.viewCounts += 1
+      restaurant.save()
       return res.render('restaurant', {
         restaurant: restaurant,
         isFavorited: isFavorited
       })
     })
   },
+
+  getDashboard: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        Comment,
+        { model: db.User, as: 'FavoritedUsers' }
+      ]
+    })
+      .then(restaurant => {
+        console.log(restaurant)
+        res.render('dashboard', { restaurant: restaurant })
+      })
+  },
+
   getFeeds: (req, res) => {
     return Restaurant.findAll({
       limit: 10,
